0c0e84d6ad744c6891c52bd10548820b516cbdc5
ANSHUL CHAURASIYA <156014121+chaurasiyanshul@users.noreply.github.com>
2025-01-06 09:41:02 +0000

    strings

---
+++ 01-basics/05-strings.js
@@ -0,0 +1,28 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " value ");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hiteshhc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0,4);
+// console.log(newString);
+
+const newStringOne = "    hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20chaoudhary"
+// console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'))
