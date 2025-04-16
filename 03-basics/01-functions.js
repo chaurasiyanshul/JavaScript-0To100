b00f15ee9c37c174870fcc0ae9d8fc29e35d47df
ANSHUL CHAURASIYA <156014121+chaurasiyanshul@users.noreply.github.com>
2025-04-16 09:52:50 +0000

    functions and objects

--- 03-basics/01-functions.js
+++ 03-basics/01-functions.js
@@ -1,23 +1,61 @@
 //  Functions
 
 
-function sayMyName(){
-    console.log("H")
-    console.log("i")
-    console.log("t")
-    console.log("e")
-    console.log("s")
-    console.log("H")
+// function sayMyName(){
+//     console.log("H")
+//     console.log("i")
+//     console.log("t")
+//     console.log("e")
+//     console.log("s")
+//     console.log("H")
+// }
+
+// sayMyName()
+
+function addTwoNumbers(num1, num2){ // parameters 
+  return num1 + num2 // 
+  // console.log(num1 + num2)
+
 }
 
-sayMyName()
+const result = addTwoNumbers(3,5)
+// console.log("Result:", result)
+// addTwoNumbers(3,null)
+// addTwoNumbers(3,"a")
 
-function addTwoNumbers(num1, num2){
-    console.log(num1 + num2)
+
+
+// function loginUserMsg(username){
+//   return `${username} Just Logged in `
+// }
+
+// loginUserMsg("Anshul")   // function chalega par return kuch nahi karega
+// console.log(loginUserMsg("kashish")) // function chalega bhi aur print bhi karega
+// if we call function but can't given arguements the function will return undefined
+
+// console.log(loginUserMsg()) // this will return undefind
+
+function loginUserMsg(username= "Betu"){  // if we define here user name then if we do not give username arguement when we call function this will give a functions but if we give function arguements this will simply over-ride it
+  if (!username){
+    console.log("Please enter Username")
+    return
+  }
+  return `${username} Just Logged in `
 
 }
-addTwoNumbers(3,null)
-addTwoNumbers(3,"a")
+
+console.log(loginUserMsg("kashish"))
+console.log(loginUserMsg())
+
+
+
+
+
+
+
+
+
+
 
 function test() {
     return typeof arguments;
@@ -31,8 +69,34 @@ if (x = 0) {
   console.log("False");
 }
 
-let a = [1, 2, 3];
-b.push(4);
-console.log(a);
-let b = a;
-console.log(b)
+// let a = [1, 2, 3];
+// b.push(4);
+// console.log(a);
+// let b = a;
+// console.log(b)
+
+
+// some time we have not idea that how many arguements will come 
+// like shopping cart 
+// we just add and add 
+
+function calculateShoppingCart(...num1){  // ...num1 this will call as rust operator in parameters ******* same as spread operator but usecase is different                                    // 
+  return num1
+  
+}
+
+console.log(calculateShoppingCart(100,200,500))
+
+
+const user = {
+  "username" : "Kashish",
+  "price" : 69
+}
+
+function handleObject(anyObject){
+  console.log(`Username is ${anyObject.username} and Price is ${anyObject.price}`)
+}
+
+handleObject(user)
+
+
